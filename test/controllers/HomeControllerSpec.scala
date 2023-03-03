1d7692ad12e7772ad1970fcfb55218b50adeffda
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-03-03 23:03:45 +0000

    Call the correct method in the HomeController test

--- test/controllers/HomeControllerSpec.scala
+++ test/controllers/HomeControllerSpec.scala
@@ -1,15 +1,15 @@
 package controllers
 
 import play.api.test.FakeRequest
-import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, stubControllerComponents}
+import play.api.test.Helpers.{GET, contentAsString, defaultAwaitTimeout, stubMessagesControllerComponents}
 import utils.PianoLessonsUtils
 
 class HomeControllerSpec extends PianoLessonsUtils {
   "HomeController contact" should {
     "render the home page" in {
-      val controller = new ContactController(stubControllerComponents())
-      val response = controller.contact()(FakeRequest(GET, "/contact"))
-      contentAsString(response) must include("piano")
+      val controller = new HomeController(stubMessagesControllerComponents())
+      val response = controller.index()(FakeRequest(GET, "/"))
+      contentAsString(response) must include("Piano")
     }
   }
 }
