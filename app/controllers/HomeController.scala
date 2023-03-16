8f5bcbd06b1f85acca92194e46fbbbf98579bf85
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2023-03-16 19:14:44 +0000

    Add a why piano section

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -6,7 +6,11 @@ import javax.inject._
 @Singleton
 class HomeController @Inject()(val controllerComponents: MessagesControllerComponents) extends MessagesBaseController {
 
-  def index() = Action { implicit request: MessagesRequest[AnyContent] =>
+  def index(): Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
     Ok(views.html.index())
+  }
+
+  def why(): Action[AnyContent] = Action { implicit request: MessagesRequest[AnyContent] =>
+    Ok(views.html.whyPiano())
   }
 }
