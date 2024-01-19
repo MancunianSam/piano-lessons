00356e91e579a79eb9a74997bf294463b5e9b1e7
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2024-01-19 16:58:42 +0000

    Add business policies

---
+++ test/controllers/PoliciesControllerSpec.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.test.FakeRequest
+import play.api.test.Helpers._
+import utils.PianoLessonsUtils
+
+class PoliciesControllerSpec extends PianoLessonsUtils {
+
+  "the policies page" should {
+    "render the policies" in {
+      val response = new PoliciesController(stubMessagesControllerComponents()).index()
+        .apply(FakeRequest(GET, "/choose-length"))
+      contentAsString(response) must include("Business Policies")
+    }
+  }
+
+}
